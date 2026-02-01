187ab0c50a6d10b3fe50e9f5b6a3f564a7be23df
Houssem Rezgui <rezguihoussem67@gmail.com>
2026-02-01 13:36:50 +0100

    Premier commit - projet initial

---
+++ src/main/java/com/morphology/arabic_morphology/ArabicMorphologyApplication.java
@@ -0,0 +1,13 @@
+package com.morphology.arabic_morphology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabicMorphologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabicMorphologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morphology/arabic_morphology/ArabicMorphologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morphology.arabic_morphology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArabicMorphologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
